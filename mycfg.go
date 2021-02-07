617962b0e6b2fb36708d4125666c9a8be5b2a4cc
Artjom Plaunov <artyemnyc@gmail.com>
2021-02-07 03:11:05 -0500

    expandtab

--- mycfg.go
+++ mycfg.go
@@ -1,134 +1,133 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"os"
-	"io/ioutil"
+  "encoding/json"
+  "fmt"
+  "os"
+  "io/ioutil"
 )
 
 type Prog struct {
-	Functions []Function
+  Functions []Function
 }
 
 type Function struct {
-	Instrs	[]Instruction
-	Name		string
+  Instrs  []Instruction
+  Name    string
 }
 
 type Instruction struct {
-	Dest		string
-	Op			string
-	Type		string
-	Value		int
-	Labels	[]string
-	Label		string
-	Args		[]string
+  Dest    string
+  Op      string
+  Type    string
+  Value   int
+  Labels  []string
+  Label   string
+  Args    []string
 }
 
 type Block struct {
-	Instrs	[]Instruction
+  Instrs  []Instruction
 }
 
 var Terminators = map[string]bool {
-	"jmp": true,
-	"ret": true,
-	"br": true,
+  "jmp": true,
+  "ret": true,
+  "br": true,
 }
 
 func main() {
-	var prog Prog
-	text, _ := ioutil.ReadFile(os.Args[1])
-	json.Unmarshal(text, &prog)
+  var prog Prog
+  text, _ := ioutil.ReadFile(os.Args[1])
+  json.Unmarshal(text, &prog)
 
-	var blocks []Block
-	for _,f := range prog.Functions {
-		blocks = create_blocks(f.Instrs)
-	}
-	block_map, names := create_block_map(blocks)
-	for key,val := range(block_map){
-		fmt.Println(key,val)
-	}
+  var blocks []Block
+  for _,f := range prog.Functions {
+    blocks = create_blocks(f.Instrs)
+  }
+  block_map, names := create_block_map(blocks)
+  for key,val := range(block_map){
+	  fmt.Println(key,val)
+  }
 
-	cfg := create_cfg(block_map, names)
-	fmt.Println(cfg)
+  cfg := create_cfg(block_map, names)
+  fmt.Println(cfg)
 }
 
-func create_cfg(block_map map[string]Block,
-								names []string) map[string][]string {
+func create_cfg(block_map map[string]Block, names []string) map[string][]string{
 
-	res := make(map[string][]string)
+  res := make(map[string][]string)
 
-	for name, block := range(block_map) {
-		res[name] = make([]string,0)
-		last := block.Instrs[len(block.Instrs)-1]
-		if last.Op == "jmp" || last.Op == "br" {
-			for _,l := range last.Labels {
-				res[name] = append(res[name],l)
-			}
-		} else if last.Op == "ret" {
-			;
-		} else {
-			i := 0
-			for names[i] != name {
-				i += 1
-			}
-			if i < len(names)-1 {
-				res[name] = append(res[name], names[i+1])
-			}
-		}
-	}
-	return res
+  for name, block := range(block_map) {
+    res[name] = make([]string,0)
+    last := block.Instrs[len(block.Instrs)-1]
+    if last.Op == "jmp" || last.Op == "br" {
+      for _,l := range last.Labels {
+        res[name] = append(res[name],l)
+      }
+    } else if last.Op == "ret" {
+      ;
+    } else {
+      i := 0
+      for names[i] != name {
+        i += 1
+      }
+      if i < len(names)-1 {
+        res[name] = append(res[name], names[i+1])
+      }
+    }
+  }
+  return res
 }
 
 func create_blocks(instrs []Instruction) []Block {
-	res := make([]Block, 0)
-	cur_block := Block{make([]Instruction,0)}
+  res := make([]Block, 0)
+  cur_block := Block{make([]Instruction,0)}
 
-	for _,instr := range instrs {
-		// An actual instruction.
-		if len(instr.Op) > 0 {
-			cur_block.Instrs = append(cur_block.Instrs, instr)
+  for _,instr := range instrs {
+    // An actual instruction.
+    if len(instr.Op) > 0 {
+      cur_block.Instrs = append(cur_block.Instrs, instr)
 
-			// Check for terminator.
-			if Terminators[instr.Op] == true {
-				res = append(res, cur_block)
-				cur_block = Block{make([]Instruction,0)}
-			}
-		// Label.
-		} else {
-			res = append(res, cur_block)
-			cur_block = Block{make([]Instruction,0)}
-			// Append label to start of new basic block.
-			cur_block.Instrs = append(cur_block.Instrs, instr)
-		}
-	}
+      // Check for terminator.
+      if Terminators[instr.Op] == true {
+        res = append(res, cur_block)
+        cur_block = Block{make([]Instruction,0)}
+      }
+    // Label.
+    } else {
+      res = append(res, cur_block)
+      cur_block = Block{make([]Instruction,0)}
+      // Append label to start of new basic block.
+      cur_block.Instrs = append(cur_block.Instrs, instr)
+    }
+  }
 
-	if len(cur_block.Instrs) > 0 {
-		res = append(res, cur_block)
-	}
-	return res
+  if len(cur_block.Instrs) > 0 {
+    res = append(res, cur_block)
+  }
+  return res
 }
 
 func create_block_map(blocks []Block) (map[string]Block, []string) {
 
-	names := make([]string, 0)
-	res := make(map[string]Block)
-	var name string
-	id := 0
+  names := make([]string, 0)
+  res := make(map[string]Block)
+  var name string
+  id := 0
 
-	for _,block := range blocks {
-		if len(block.Instrs[0].Label) > 0 {
-			name = block.Instrs[0].Label
-			res[name] = Block{block.Instrs[1:]}
-		} else {
-			name = fmt.Sprintf("b%d", id)
-			id += 1
-			res[name] = block
-		}
-		names = append(names,name)
-	}
-	return res, names
+  for _,block := range blocks {
+    if len(block.Instrs[0].Label) > 0 {
+      name = block.Instrs[0].Label
+      res[name] = Block{block.Instrs[1:]}
+    } else {
+      name = fmt.Sprintf("b%d", id)
+      id += 1
+      res[name] = block
+    }
+    names = append(names,name)
+  }
+  return res, names
 }
 
 
